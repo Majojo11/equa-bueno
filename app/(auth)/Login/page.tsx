0bf40d83cc93608f2fbb68632cfce29f9d2085da
Majojo11 <mresendiz99@alumnos.uaq.mx>
2025-10-24 16:39:39 -0600

    proyecto base

---
+++ app/(auth)/Login/page.tsx
@@ -0,0 +1,53 @@
+"use client";
+import { API_URL } from "@/constants";
+import { Button, Input, Link, Spinner } from "@heroui/react";
+import { useState } from "react";
+import  { useRouter } from "next/navigation";
+
+export default function LoginPage(){
+    const [submitting, setSubmitting] = useState(false)
+    const router = useRouter()
+    const handleSubmit = async (e: React.FormEvent) => {
+        setSubmitting(true);
+        e.preventDefault()
+        const formData = new FormData(e.target)
+        let authData: any = {}
+        authData.userEmail = formData.get("userEmail")
+        authData.userPassword = formData.get("userPassword")
+        try{
+            const response = await fetch(`${API_URL}/auth/login`,{
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(authData),
+                credentials: "include",
+            })
+            if(response.status === 201) router.push(`/dashboard`)
+            setSubmitting(false);
+        }catch (e) {
+            setSubmitting(false);
+        }
+        return;
+    }
+    return (
+    <form className="bg-orange-500 px-10 py-2 rounded-md" onSubmit={handleSubmit}>
+        <p className="text-2xl my-4 text-white">Iniciar sesión</p>
+
+        <div className="flex flex-col gap-2 my-4 items-center">
+            <Input label="Email" name="userEmail" type="email" isRequired={true} size="md"/>
+            <Input label="Contraseña" name="userPassword" type="password" isRequired={true} size="md"/>
+        </div>
+
+        <div className="flex flex-col gap-2 items-center">
+            <Button 
+            color="primary" 
+            type="submit"
+            disabled={submitting}>
+                {submitting? <Spinner size='md'/> : "Iniciar sesión"}
+            </Button>
+            <p className="text-white">¿No tienes cuenta? <Link href="/signup" className="text-red-600 underline">Registrate</Link></p>
+        </div>
+    </form>
+    )
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@heroui/react';
+export default function App() {
+  return (
+    <div className="bg-blue-500 text-white p-6 rounded-lg">
+      <h1 className="text-3xl font-bold">Tailwind está funcionando 🚀</h1>
+      <Button className="mt-4" color="primary">¡Haz clic aquí!</Button>
+    </div>
+  );
+}
